4daa14ced1f4e18238bce1b8ce3abf4073aa80e1
Bruno Alves de Araujo <88730176+brunodev21@users.noreply.github.com>
2022-11-04 17:57:34 -0300

    segundo commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
-const { request, response } = require("express");
 const express = require("express");
+const { v4: uuidv4 } = require("uuid")
 
 const app = express();
 
+app.use(express.json());
+
+const customers = [];
+
 app.post("/account", (request, response) => {
-  const cpf = request.body;
+  const {cpf, name } = request.body;
+
+  const costumeAlreadyExists = customers.some((customer) => customer.cpf === cpf);
+
+  const id = uuidv4();
+
+  customers.push({
+    cpf,
+    name,
+    id,
+    statement: []
+  });
   
-})
+  return response.status(201).send();
+});
 
 app.listen(3333);

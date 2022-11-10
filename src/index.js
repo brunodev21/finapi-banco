7c1d3f8f3b52062248de7374a263640cbaae0733
Bruno Alves de Araujo <88730176+brunodev21@users.noreply.github.com>
2022-11-10 16:49:20 -0300

    primeiro commit

--- src/index.js
+++ src/index.js
@@ -58,6 +58,7 @@ app.post("/account", (request, response) => {
 
 app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
   const { customer } = request;
+
   return response.json(customer.statement);
 });
 
@@ -97,6 +98,17 @@ app.post("/withdraw", verifyIfExistsAccountCPF, (request, response) => {
   customer.statement.push(statementOperation);
 
   return response.status(201).send();
+});
+
+app.get("/statement/date", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+  const { date } = request.query;
+
+  const dateFormat = new Date(date + " 00:00");
+
+  const statement = customer.statement.filter((statement) => statement.created_at.toDateString() === new Date(dateFormat).toDateString());
+
+  return response.json(customer.statement);
 });
 
 app.listen(3333);
